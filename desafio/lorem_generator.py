c5775c8fb2ddebc4ef5c8038a0612cd4431a047e
arpkk <gina.ozimisa@usach.cl>
2021-10-19 07:10:39 -0300

    Fix lorem_generator.py

--- desafio/lorem_generator.py
+++ desafio/lorem_generator.py
@@ -1,1 +1,8 @@
-n=sys.argv[1]
+import sys
+n=int(sys.argv[1])
+
+parrafo= "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque malesuada felis ac purus placerat, sit amet molestie nisl ullamcorper. Aliquam lacinia neque egestas nisi blandit lobortis. Aenean ac gravida lorem. Aliquam blandit lectus ut eros semper, non auctor velit accumsan. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc accumsan quam arcu, quis convallis sapien imperdiet sed. Donec faucibus lacus quam, vel pretium leo iaculis sed. Nullam luctus ex at massa placerat, sit amet sollicitudin quam venenatis. Proin nisi enim, viverra ut ipsum ac, facilisis scelerisque risus. Praesent bibendum sem in est egestas scelerisque. Vivamus molestie felis ipsum, eu convallis metus iaculis et."
+
+for i in range(n):
+    print(parrafo)
+    print("")
